94d896ce9df424ab91fb3b60b41edc6b9c69722e
epsil2 <35408884+epsil2@users.noreply.github.com>
2025-01-29 18:43:38 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,29 @@ import sqlite3
 import pandas_ta as ta
 import plotly.graph_objs as go
 from datetime import datetime, timedelta
+import sqlite3
 
+def create_table():
+    conn = sqlite3.connect('stocks.db')
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS stocks (
+            symbol TEXT NOT NULL,
+            date DATETIME NOT NULL,
+            open REAL,
+            high REAL,
+            low REAL,
+            close REAL,
+            adj_close REAL,
+            volume REAL,
+            PRIMARY KEY (symbol, date)
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    create_table()
 # ================== PAGE CONFIG ==================
 st.set_page_config(page_title="Stock Analyzer Pro", layout="wide")
 
